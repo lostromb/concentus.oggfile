e9225ff6f1e32533575552117bfa5044bd5bb6ec
Martin Cvengros <mcv618@gmail.com>
2021-10-29 23:53:48 +0200

    rm non src 4 Unity

--- Concentus.Oggfile/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Resources;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
