34a1133a97b854814e592772ba922094c815ab39
Martin Cvengros <mcv618@gmail.com>
2022-02-17 08:33:57 +0100

    Revert "rm non src 4 Unity"
    This reverts commit e9225ff6f1e32533575552117bfa5044bd5bb6ec.

---
+++ Concentus.Oggfile/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
