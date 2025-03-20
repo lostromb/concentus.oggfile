5bdcc465c5fe2a1672e9bdf725a47de3d540d64d
Peter Jarrett <peterj@broadcastradio.com>
2025-03-20 08:49:10 +0000

    Resolved issue with Container Reader not being closed/disposed. Suggested increment version to 1.0.6.0 Updated to depend on Concentus 2.2.2

--- Concentus.Oggfile/OpusHeader.cs
+++ Concentus.Oggfile/OpusHeader.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#pragma warning disable 0169
+
 namespace Concentus.Oggfile
 {
     internal class OpusHeader
@@ -18,3 +20,5 @@ namespace Concentus.Oggfile
         byte coupled_count;
     }
 }
+
+#pragma warning restore 0169
